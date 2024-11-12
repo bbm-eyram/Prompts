b28d8556640e5dbec7900f0b5504ab5d1ce1c3f5
bbm-eyram <133222520+bbm-eyram@users.noreply.github.com>
2024-11-12 10:43:32 +0000

    prompts

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+}
+
+// Use ES module export syntax instead of module.exports
+export default nextConfig;
