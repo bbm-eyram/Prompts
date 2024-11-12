6cfac205466fc19b65ade2cc13516cb3e9955955
bbm-eyram <133222520+bbm-eyram@users.noreply.github.com>
2024-11-12 10:50:22 +0000

    prompts

---
+++ app/layout.jsx
@@ -0,0 +1,33 @@
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <head>
+      <title>{metadata.title}</title>
+      <meta name="description" content={metadata.description} />
+      <link rel="icon" href="/assets/images/logo.svg" type="image/x-icon" />
+    </head>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
