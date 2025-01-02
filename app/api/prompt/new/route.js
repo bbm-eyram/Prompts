7a13dbfe1f32cc42746dae256817b8fd63557089
bbm-eyram <133222520+bbm-eyram@users.noreply.github.com>
2025-01-02 15:37:27 +0000

    Update route.js

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,18 +1,16 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
 export const POST = async (request) => {
+    const { userId, prompt, tag } = await request.json();
+
     try {
-        const { userId, prompt, tag } = await request.json();
-
-        console.log("Incoming Data:", { userId, prompt, tag }); // Log request payload
-
         await connectToDB();
         const newPrompt = new Prompt({ creator: userId, prompt, tag });
 
         await newPrompt.save();
-        console.log("New Prompt Saved:", newPrompt); // Log saved prompt
-
-        return new Response(JSON.stringify(newPrompt), { status: 201 });
+        return new Response(JSON.stringify(newPrompt), { status: 201 })
     } catch (error) {
-        console.error("Error in API:", error); // Log error details
         return new Response("Failed to create a new prompt", { status: 500 });
     }
-};
+}  
